92149c29354029071ae81f81dc6c9964e7dc5fcd
Xirvin <xander228irvin@gmail.com>
2022-11-16 22:26:57 +1100

    Add client or sever event check

--- client/client.lua
+++ client/client.lua
@@ -1,14 +1,15 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local isOpen = false
 
-RegisterCommand('dev', function()
+RegisterCommand('xv-dev:toggleMenu', function()
+    isOpen = not isOpen
     SendNUIMessage({
         action = "DevMenu",
         data = {
-            show = true,
+            show = isOpen,
         }
     })
-    SetNuiFocus(true, true)
-end, false)
+    SetNuiFocus(isOpen, isOpen)
+end, 'admin')
 
 RegisterNUICallback('hideUI', function(data, cb)
     SendNUIMessage({
@@ -22,18 +23,22 @@ end)
 
 RegisterNUICallback('ExecuteLua', function(data, cb)
     local code = data.code
-    local func, err = load(code)
-    if func then
-        local status, result = pcall(func)
-        if not status then
-            print(result)
-            cb({status = false, result = result})
+    local eventType = data.eventType
+    if eventType == 'client' then
+        local func, err = load(code)
+        if func then
+            local status, result = pcall(func)
+            if not status then
+                print(result)
+            end
+        else
+            print(err)
         end
-    else
-        print(err)
-        cb(err)
+    elseif eventType == 'server' then
+        --Create a callback for the server event
     end
 end)
+
 
 RegisterNetEvent('xirvin-test', function()
     print('test')
