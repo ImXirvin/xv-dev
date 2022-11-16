fbcc59fde6277936b64e197eef2a1d67253f5e33
Xirvin <103050557+ImXirvin@users.noreply.github.com>
2022-11-16 15:34:13 +1100

    Add Sidebar button (not working)

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -2,14 +2,13 @@
   import Drawer from "./components/Drawer.svelte";
   import VisibilityProvider from "./providers/VisibilityProvider.svelte";
   import { visibility } from "./store/stores";
+  import { debugData } from "./utils/debugData";
   import { ReceiveNUI } from "./utils/ReceiveNUI";
 
   ReceiveNUI("DevMenu", (data) => {
     visibility.set(data.show);
-
   });
-
-
+  visibility.set(true);
 </script>
 
 <VisibilityProvider>
--- ui/src/components/Drawer.svelte
+++ ui/src/components/Drawer.svelte
@@ -13,13 +13,13 @@
 </script>
 
 
-<div class="main-container absolute w-[45rem] h-[90%] bottom-0 right-0 bg-black rounded-tl-[3rem] flex flex-col">
-    <span class="relative h-[2rem] grid place-items-center p-1 mb-3 align-center">
-        <h1 class="text-white text-3xl font-bold relative">Xirvin's VERY USEFUL Dev Menu</h1>
+<div class="main-container absolute w-[45rem] h-[90%] bottom-0 right-0 bg-black  flex flex-col">
+    <span class="relative h-[5rem] flex flex-row p-1 mb-3 items-center top-3">
+        <span class="relative text-white  text-[2.5rem] grid place-content-center sidebar-button fas fa-list z-10"></span>
+        <h1 class="text-white text-3xl font-bold absolute text-center w-full z-0">Xirvin's Dev Menu</h1>
     </span>
     <div class="h-full  relative flex flex-row">
         <!-- LIST  -->
-
         <ol class="w-[25%] h-full self-end relative text-center gap-5 flex flex-col">
             {#each options as option, i}
             <li class="list-item text-white text-2xl font-bold py-5" on:click={() => selected = options[i]}>{option.name}</li>
@@ -42,9 +42,25 @@
         --color-secondary: rgba(0, 0, 0, 0.8);
     }
 
+    .sidebar-button {
+        background-color: none;
+        border-radius: 1rem;
+        cursor: pointer;
+        /* top: 1rem;
+        left: 1rem; */
+        margin: 1rem;
+        padding: 1rem
+    }
+
+    .sidebar-button:hover {
+        background-color: rgba(255, 255, 255, 0.1);
+    }
+
+
     .main-container {
         box-shadow: 0 0 0.5rem 0.2rem rgba(0, 0, 0, 0.5);
         background-color: var(--color-primary);
+        border-radius: 1rem;
     }
 
 
