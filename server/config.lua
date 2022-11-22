34f89e85a0a2d583fffbefc221ba3e33fa61eb9c
Xirvin <xander228irvin@gmail.com>
2022-11-22 18:01:14 +1100

    Update config comment

--- server/config.lua
+++ server/config.lua
@@ -3,9 +3,9 @@ Config = {
 
     Identifiers  = { -- Identifiers that will be allowed to use the menu if StrictMode is true.
         {
-            licenses = {
-            "steam:examplehex", -- Get using txadmin
-            "license:exampleLicense",
+            licenses = {-- Get using txadmin, can also use any identifier that fivem provides xbl, ip, etc
+            "steam:examplehex", 
+            "license:exampleLicense", 
             },
             client = true, --Allowed to execute client side code
             server = false, --Allowed to execute server side code
