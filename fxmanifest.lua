93966262b53cba5cd9abfd2f906b2391271428bb
Xirvin <xander228irvin@gmail.com>
2022-12-27 22:59:19 +1100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,38 +1,36 @@
-fx_version 'cerulean'
+fx_version("cerulean")
 
-game "gta5"
+game("gta5")
 
-name 'xv-dev'
-author 'Xirvin#0985'
-description "FiveM Development Menu."
-version '2.2.1'
-repository 'https://github.com/ImXirvin/xv-dev'
+name("xv-dev")
+author("Xirvin#0985")
+description("FiveM Development Menu.")
+version("2.2.2")
+repository("https://github.com/ImXirvin/xv-dev")
 
+ui_page("html/index.html")
 
+shared_scripts({
+	"@ox_lib/init.lua",
+	"shared/lib.lua",
+})
 
-ui_page 'html/index.html'
+client_script({
+	"client/client.lua",
+	"client/resources.lua",
+	"client/players.lua",
+})
+server_script({
+	"server/config.lua",
+	"server/server.lua",
+	"server/resources.lua",
+	"server/players.lua",
+})
 
-shared_scripts {
-  '@ox_lib/init.lua',
-  'shared/lib.lua',
-}
+files({
+	"html/index.html",
+	"html/index.js",
+	"html/index.css",
+})
 
-client_script {
-  'client/client.lua',
-  'client/resources.lua',
-  'client/players.lua',
-}
-server_script {
-  "server/config.lua",
-  "server/server.lua",
-  "server/resources.lua",
-  "server/players.lua",
-}
-
-files {
-  'html/index.html',
-  'html/index.js',
-  'html/index.css',
-}
-
-lua54 'yes'
+lua54("yes")
